e43e47cd63d34b252148df2056bb4474f930a66b
QiaoBo <374264192@qq.com>
2017-12-25 12:19:09 +0800

    first commit + iv

--- app/src/main/java/net/bawei/gitdemo/MainActivity.java
+++ app/src/main/java/net/bawei/gitdemo/MainActivity.java
@@ -2,11 +2,13 @@ package net.bawei.gitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextView;
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextView = (TextView) findViewById(R.id.tv);
+
+        mImageView = (ImageView) findViewById(R.id.iv);
 
     }
 }
